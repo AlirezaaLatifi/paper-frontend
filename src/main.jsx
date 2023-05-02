c70194c079a26e008badbe8c9225ada39b4ac5fc
alirezaa-latifi <l.alirezalatifi@gmail.com>
2023-05-02 19:30:29 +0430

    add BASE_NAME of router to environment variables

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/paper-fronend">
+  <BrowserRouter basename={import.meta.env.VITE_BASE_NAME}>
     <App />
   </BrowserRouter>
 );
