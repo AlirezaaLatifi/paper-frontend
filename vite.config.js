20395de475f55f82d9797140c725193c38796c8e
alirezaa-latifi <l.alirezalatifi@gmail.com>
2023-05-02 15:36:04 +0430

    add environment variables and set API_URL and BASE_URL

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 // import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/paper-frontend/',
-  plugins: [react()],
-  // plugins: [react(), eslint()],
-});
+export default async ({ mode }) => {
+  // Load environment variables into the Node.js process environment
+  const env = await loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    // Use environment variables in your Vite config
+    base: env.VITE_BASE_URL,
+    plugins: [react()],
+  });
+};
